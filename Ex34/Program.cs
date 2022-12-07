dd328e67f2772496575770b51a24592964a14408
Maksim <maksim_belotelov@mail.ru>
2022-12-07 21:38:54 +0300

    add Ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать массив, заполненный случайными положительными трёхзначными
+// числами. Посчитать количество четных чисел в массиве.
+
+Console.Clear();
+
+Console.Write("Введите число элементов в массиве: ");
+int size=Convert.ToInt32(Console.ReadLine());
+
+int[] array=FillArray(size, 100, 999);
+
+Console.WriteLine($"Введенный массив: [{string.Join(",",array)}]");
+Console.WriteLine($"Количество четных элементов: {evenCount(array)}");
+
+int[] FillArray(int sizeArr, int min, int max)
+{
+    Random random=new Random();
+    int[] resultArray=new int[sizeArr];
+    for(int i=0; i<resultArray.Length; i++)
+        resultArray[i]=random.Next(min, max+1);
+    return resultArray;
+}
+
+int evenCount(int [] arr)
+{
+    int counter=0;
+    for(int i=0; i<arr.Length; i++)
+        if(arr[i]%2==0)
+            counter++;
+    return counter;
+}
